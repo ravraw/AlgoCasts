694a080ef3099216c9634102aaf9c575e41f6d61
ravindra rawat <ravinrawat.wd@gmail.com>
2018-03-15 20:59:04 +0000

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,21 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+let chars = {};
+maxChar = "";
+maxCount = 0;
+  for (let char of str ){
+    !chars[char] ? chars[char] = 1 : chars[char]++ ;
+  }
+ for (let char in chars){
+  if(chars[char] > maxCount){
+    maxChar = char ;
+    maxCount = chars[char];
+  }
+ }
+return maxChar ;
+}
 
 module.exports = maxChar;
