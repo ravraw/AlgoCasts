e7ee82c1f01e1d9025f6f9012a8cf822ab02b243
ravindra rawat <ravinrawat.wd@gmail.com>
2018-03-14 20:14:26 -0600

    reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
-
+function reverse(str) {
+  return str.split("").reduce((rev, char) =>  char + rev ,"")
+}
 module.exports = reverse;
+
+
+// method 1 : str.split('').reverse().join('');
+
+// Method 2 :
+
+// let reversedString = '';
+//   for(let i = 0; i < str.length; i++) {
+//     reversedString = str[i] + reversedString ;
+//   }
+//   return reversedString ;
+
+// Method 3 :
+// let reversedString = '';
+//   for(character of str) {         // uses es6 for loop
+//     reversedString = character + reversedString ;
+//   }
+//   return reversedString ;
+
+// method: 4
+// function reverse(str) {
+//   return str.split("").reduce((rev, char) =>  char + rev ,"")
+// }
+// module.exports = reverse;
